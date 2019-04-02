a601ba10b7d1cc8f9ec65584bd63a2295a1fb46d
another <2036023557@qq.com>
2019-04-02 19:05:59 +0800

    zcf5.0

--- js/u-personal.js
+++ js/u-personal.js
@@ -1,22 +1,30 @@
 $(function () {
     // console.log($("body"))
-    $('.alter').click(function () {
+    $(document).scrollTop(10)
+    if ($(document).scrollTop() > 0) {
+        $('.foot').css("position", "initial")
+    }
+    $('.alter').click(function (e) {
+        e.preventDefault();
         $(this).css("display", "none")
-        $('.infoitem-content').eq($('.alter').index(this)*2).css("display", "none");
+        $('.infoitem-content').eq($('.alter').index(this) * 2).css("display", "none");
         $('.infoalter').eq($('.alter').index(this)).css("display", "block");
         $(document).scrollTop(10)
-        if( $(document).scrollTop()>0 ){
-            $('.foot').css("position","initial")
+        if ($(document).scrollTop() > 0) {
+            $('.foot').css("position", "initial")
         }
     });
-    $('.user-btn-cancel').click(function(){
+    $('.user-btn-cancel').click(function (e) {
+        e.preventDefault();
         $(document).scrollTop(10)
-        if( $(document).scrollTop()>0 ){
-            $('.foot').css("position","fixed")
+        if ($(document).scrollTop() > 0) {
+            $('.foot').css("position", "fixed")
         }
-        $('.infoitem-content').eq($('.user-btn-cancel').index(this)*2).css("display", "block");
+        $('.infoitem-content').eq($('.user-btn-cancel').index(this) * 2).css("display", "block");
         $('.infoalter').eq($('.user-btn-cancel').index(this)).css("display", "none");
-        $('.alter').eq($('.user-btn-cancel').index(this)).css("display", "block");    
+        $('.alter').eq($('.user-btn-cancel').index(this)).css("display", "block");
     })
-    
+    $('.skillshint em').click(function () {
+        $(this).parent().hide();
+    })
 })
