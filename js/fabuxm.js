7c354a77914f4e53552dc481717774d5a32d24c9
Your Name <944619458@qq.com>
2019-04-12 00:28:52 +0800

    0412

--- js/fabuxm.js
+++ js/fabuxm.js
@@ -59,7 +59,6 @@ $(function(){
             <div class="san_ybian col-md-6 ">
                 <div class="san_btn ">
                     <button class="qux" style=" display:`+jeson[i].yingcyi+`">取消发布</button>
-                    <button class="bianji" style=" display:`+jeson[i].yingcer+`">编辑项目</button>
                     <button class="xmuzt">查看项目状态</button>
                     <div class="cebla"></div>
                 </div>
@@ -85,6 +84,7 @@ $(function(){
             //     $(".dierge").append(odiv);
             // }
             $(".xiala li").on("click",function(){
+
                 var _this=$(this);
                 $(".dierge>div").remove()
                 $.each(jeson,function(i,n){
@@ -151,6 +151,65 @@ $(function(){
                 $(".dierge").append(odiv);
                 console.log($(".dierge"))
 
+                    }else if(_this.text() == "全部状态"){
+                        var odiv = $(`
+                        <div class="dierge_er ">
+                            <div class="hezizj row">
+                                <!-- hezizj是包上面的盒子 -->
+                                <div class="hezizj_zuo col-md-6 col-xs-5">
+                                    <h3>NO.<span style="font-size: 15px">`+ jeson[i].bianhao+`</span> <span>`+jeson[i].name+`</span></h3>
+                                </div>
+                                <div class="hezizj_yb col-md-6 col-xs-7">
+                                    <!-- 右 -->
+                                    <div class="tutu">
+                                        <img src="img/liulanl.png" alt="">
+                                    </div>
+                                    <span>浏览量 <span>`+ jeson[i].liulan+`</span> </span>
+                                    <div class="tutu">
+                                        <img src="img/jusho (1).png" alt="">
+                                    </div>
+                                    <span>报名数 5 </span>
+                                    <span> | </span>
+                                    <span style="color:`+ jeson[i].col +`"><span class="zhuantaio">`+jeson[i].zhuangtai+`</span></span>
+                                </div>
+                                <div class="cebla"></div>
+                            </div>
+                            <div class="dierge_san row">
+                                <div class="san_zuo col-md-6 row">
+                                    <div class="zuo_tu col-md-6 col-xs-6">
+                                        <img src="img/`+jeson[i].img +`" alt="">
+                                    </div>
+                                    <div class="tishi col-md-6 col-xs-6 ">
+                                        <div class="tishi_yi">
+                                            <span>`+jeson[i].kaifa+`</span>
+                                            <span></span>
+                                        </div>
+                                        <div class="tishi_er ">
+                                            <p>
+                                                <span>金额</span>
+                                                ￥<span>`+jeson[i].money +`</span> |
+                                            </p>
+                                            <p>
+                                                <span>周期</span>
+                                                <span>`+jeson[i].zhoqi+`</span>天 |
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="san_ybian col-md-6 ">
+                                    <div class="san_btn ">
+                                        <button class="qux" style=" display:`+jeson[i].yingcyi+`">取消发布</button>
+                                        <button class="bianji" style=" display:`+jeson[i].yingcer+`">编辑项目</button>
+                                        <button class="xmuzt">查看项目状态</button>
+                                        <div class="cebla"></div>
+                                    </div>
+                                </div>
+                                <div class="cebla"></div>
+                            </div>
+                        </div>
+                                    `);
+                                    
+                                    $(".dierge").append(odiv);
                     }
                     $(".qux").click(function(){
                         $(this).css("display","none");
--- js/kfzym.js
+++ js/kfzym.js
@@ -1,11 +1,19 @@
 $(function(){
+
+    // $(".bti span").eq(0).css("color","red");
+    $(".shoruo").css("color","red");
+
+
     $(".bti span").click(function(){
+        $(".shoruo").css("color","");
         $(this).addClass("shoruli").siblings().removeClass("shoruli");
-        $(".genz>div").eq(0).show();//默认让第一个div显示
-        $(".genz>div").hide();//所有div隐藏
-        var a=$(this).index();//得到当前执行事件标签的未知数
-        $(".genz>div").eq(a).show();//让对应位置的div显示
+        // $(this).eq(0).removeClass("shoruli");//默认让第一个div显示
+      
     });
+
+    
+
+
 
     $.ajax({
         url:"../json/json2.json",
@@ -23,12 +31,13 @@ $(function(){
 
             function xuanran(){
                for(var i=0;i<jeson.length;i++){
+                   var imgi = i+1;
                 var odiv = $(
                     `
                  <div class="col-md-6">
                 <div class="kfzhe ">
                     <div class="kfzhe_yi">
-                        <img src="img/di1.png" alt="">
+                        <img src="img/`+imgi+`.png" alt="">
                     </div>
 
                     <div class="kfzhe_er">
@@ -45,6 +54,7 @@ $(function(){
                            <div class="shoru">
                                总收入： 
                                <strong>￥<span class="shuziyi">`+jeson[i].money+`</span></strong>
+                               <p style="display: inline-block; padding: 0 3px 0 5px">评分:<span>`+jeson[i].assess+`</span> </p>
                            </div>
                     </div>
                 </div>
@@ -55,132 +65,173 @@ $(function(){
                 $(".paiyi").append(odiv);
                 
             } 
-            }
-            
-
             $(".shoruo").click(function (e) { 
                 e.preventDefault();
-                function sortassess(a,b){
-                    return a.money-b.money
+                function sortmoney(a,b){
+                    return b.money-a.money
                 }
                 jeson.sort(sortmoney);
-                // $(".paiyi").remove();
+                $(".paiyi").children().remove();
+                // $("div").remove(".paiyi");
                 xuanran();
             });
-
             $(".shurogh").click(function (e) { 
                 e.preventDefault();
                 function sortassess(a,b){
-                    return a.money-b.money
+                    return b.assess-a.assess
                 }
-                jeson.sort(sortmoney);
-                // $(".paier").remove();
+                jeson.sort(sortassess);
+                $(".paiyi").children().remove();
                 xuanran();
             });
-            // selector
             $(".shoghj").click(function (e) { 
                 e.preventDefault();
                 function sortxiangmu(a,b){
-                    return a.cishu-b.cishu
+                    return b.xiangmu-a.xiangmu
                 }
-                jeson.sort(sortmoney);
-                // $(".paisan").remove();
+                jeson.sort(sortxiangmu);
+                $(".paiyi").children().remove();
+                // $(".paiyi").remove();
                 xuanran();
             });
-            
-            
+        } 
+    
         },
+
         
-
     });
     
     
 
-    $.ajax({
-        url:"../json/json2.json",
-        type:"GET",
-        dataType:"json",
-        success:function(result){
-            var jeson = result.dataer;
-            for(var i=0;i<jeson.length;i++){
-                var odiv = $(
-                    `
-                 <div class="col-md-6">
-                <div class="kfzhe ">
-                    <div class="kfzhe_yi">
-                        <img src="img/di1.png" alt="">
-                    </div>
+    // $.ajax({
+    //     url:"../json/json2.json",
+    //     type:"GET",
+    //     dataType:"json",
+    //     success:function(result){
+    //         var jeson = result.dataer;
+    //         function sortmoney(a,b){
+    //             return a.money-b.money
+    //         }
+    //         jeson.sort(sortmoney);
+    //         // $(".paier").remove();
+    //         xuanran();
 
-                    <div class="kfzhe_er">
-                         <img src="https://dn-coding-net-production-static.codehub.cn/cb6b9c4c-2f22-4aec-a1db-7d373371c570.jpg?imageMogr2/auto-orient/format/jpeg/crop/!128x128a0a0" alt="">
-                    </div>
+    //         function xuanran(){
+
+    //             for(var i=0;i<jeson.length;i++){
+    //                 var imgi=i+1;
+    //                 var odiv = $(
+    //                     `
+    //                 <div class="col-md-6">
+    //                 <div class="kfzhe ">
+    //                     <div class="kfzhe_yi">
+    //                         <img src="img/`+imgi+`.png" alt="">
+                            
+    //                     </div>
+
+    //                     <div class="kfzhe_er">
+    //                         <img src="https://dn-coding-net-production-static.codehub.cn/cb6b9c4c-2f22-4aec-a1db-7d373371c570.jpg?imageMogr2/auto-orient/format/jpeg/crop/!128x128a0a0" alt="">
+    //                     </div>
+                        
+    //                     <div class="kfzhe_san">  
+    //                         <div class="name">`+jeson[i].name +`</div>
+    //                         <div class="info">
+    //                             <span>最擅长：<span>`+jeson[i].sahnchang+`</span></span>
+    //                             <span>参与项目： <span>`+jeson[i].xiangmu+`</span></span>
+    //                             <span>城市： <span>`+jeson[i].chengshi+`</span></span>
+    //                         </div>
+    //                         <div class="shoru">
+    //                             总评分： 
+    //                             <strong>`+jeson[i].assess+`</strong>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //                 </div>
+
+    //                     `
+    //                 )
+    //                 $(".paier").append(odiv);
+    //             }
+    //         }
+    //         $(".shurogh").click(function (e) { 
+    //             e.preventDefault();
+    //             function sortassess(a,b){
+    //                 return a.assess-b.assess
+    //             }
+    //             jeson.sort(sortassess);
+    //             // $(".paier").remove();
+    //             // xuanran();
+    //         });
+    
+    //     }
+       
+    // });
+
+
+    // $.ajax({
+    //     url:"../json/json2.json",
+    //     type:"GET",
+    //     dataType:"json",
+    //     success:function(result){
+    //         var jeson = result.datasan;
+    //         function sortacishu(a,b){
+    //             return b.cishu-a.cishu
+    //         }
+    //         jeson.sort(sortacishu);
+    //         // $(".paisan").remove();
+    //         xuanran();
+
+    //         function xuanran(){
+    //         for(var i=0;i<jeson.length;i++){
+    //             var odiv = $(
+    //                 `
+    //              <div class="col-md-6">
+    //             <div class="kfzhe ">
+    //                 <div class="kfzhe_yi">
+    //                     <img src="img/di1.png" alt="">
+    //                 </div>
+
+    //                 <div class="kfzhe_er">
+    //                 <img src="https://coding.net/static/fruit_avatar/Fruit-20.png" alt="">
+    //                 </div>
                     
-                    <div class="kfzhe_san">  
-                           <div class="name">`+jeson[i].name +`</div>
-                           <div class="info">
-                            <span>最擅长：<span>`+jeson[i].sahnchang+`</span></span>
-                            <span>参与项目： <span>`+jeson[i].xiangmu+`</span></span>
-                            <span>城市： <span>`+jeson[i].chengshi+`</span></span>
-                           </div>
-                           <div class="shoru">
-                               总评分： 
-                               <strong>`+jeson[i].assess+`</strong>
-                           </div>
-                    </div>
-                </div>
-                </div>
+    //                 <div class="kfzhe_san">  
+    //                        <div class="name">`+jeson[i].name +`</div>
+    //                        <div class="info">
+    //                         <span>最擅长：<span>`+jeson[i].sahnchang+`</span></span>
+    //                         <span>参与项目： <span>`+jeson[i].xiangmu+`</span></span>
+    //                         <span>城市： <span>`+jeson[i].chengshi+`</span></span>
+    //                        </div>
+    //                        <div class="shoru">
+    //                            报名次数： 
+    //                            <strong>`+jeson[i].cishu+`</strong>
+    //                        </div>
+    //                 </div>
+    //             </div>
+    //             </div>
 
-                    `
-                )
-                $(".paier").append(odiv);
-            }
-        }
+    //                 `
+    //             )
+    //             $(".paisan").append(odiv);
+    //         }
+    //         $(".shoghj").click(function (e) { 
+    //             e.preventDefault();
+    //             function sortcishu(a,b){
+    //                 return a.cishu-b.cishu
+    //             }
+    //             jeson.sort(sortcishu);
+    //             // $(".paisan").remove();
+    //             // xuanran();
+    //         });
+    //     }
+    // }
 
-    });
-
-
-    $.ajax({
-        url:"../json/json2.json",
-        type:"GET",
-        dataType:"json",
-        success:function(result){
-            var jeson = result.datasan;
-            for(var i=0;i<jeson.length;i++){
-                var odiv = $(
-                    `
-                 <div class="col-md-6">
-                <div class="kfzhe ">
-                    <div class="kfzhe_yi">
-                        <img src="img/di1.png" alt="">
-                    </div>
-
-                    <div class="kfzhe_er">
-                    <img src="https://coding.net/static/fruit_avatar/Fruit-20.png" alt="">
-                    </div>
-                    
-                    <div class="kfzhe_san">  
-                           <div class="name">`+jeson[i].name +`</div>
-                           <div class="info">
-                            <span>最擅长：<span>`+jeson[i].sahnchang+`</span></span>
-                            <span>参与项目： <span>`+jeson[i].xiangmu+`</span></span>
-                            <span>城市： <span>`+jeson[i].chengshi+`</span></span>
-                           </div>
-                           <div class="shoru">
-                               报名次数： 
-                               <strong>`+jeson[i].cishu+`</strong>
-                           </div>
-                    </div>
-                </div>
-                </div>
-
-                    `
-                )
-                $(".paisan").append(odiv);
-            }
-        }
-
-    });
+    // });
 
 
+   
 
+    
+    // selector
+    
 })
